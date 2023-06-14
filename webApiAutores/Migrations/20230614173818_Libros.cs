a2ef26cb0c23af899efb1dc52eb99f4c035f6ac9
Theendisonly <lilium277@gmail.com>
2023-06-14 17:01:33 -0600

    refactor: adding LibrosController and complementing functionality for AutoresController

---
+++ webApiAutores/Migrations/20230614173818_Libros.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webApiAutores.Migrations
+{
+    /// <inheritdoc />
+    public partial class Libros : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores");
+
+            migrationBuilder.RenameTable(
+                name: "Autores",
+                newName: "Autor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor");
+
+            migrationBuilder.RenameTable(
+                name: "Autor",
+                newName: "Autores");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores",
+                column: "Id");
+        }
+    }
+}
---
+++ webApiAutores/Migrations/20230614221950_Libros_fix.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webApiAutores.Migrations
+{
+    /// <inheritdoc />
+    public partial class Libros_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor");
+
+            migrationBuilder.RenameTable(
+                name: "Autor",
+                newName: "Autores");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libros_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_AutorId",
+                table: "Libros",
+                column: "AutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libros");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Autores",
+                table: "Autores");
+
+            migrationBuilder.RenameTable(
+                name: "Autores",
+                newName: "Autor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Autor",
+                table: "Autor",
+                column: "Id");
+        }
+    }
+}
